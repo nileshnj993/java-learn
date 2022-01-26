02b06a730b463ce40cf950ca5f58d224aca9868f
Jain <Nilesh.Jain@sabre.com>
2022-01-27 01:42:25 +0530

    added new codes

---
+++ src/ifElse.java
@@ -0,0 +1,11 @@
+public class ifElse {
+    public static void main(String[] args) {
+        int num = 20;
+        if(num>15){
+            System.out.println("Greater than 15!");
+        }
+        else{
+            System.out.println("Lesser than 15!");
+        }
+    }
+}
