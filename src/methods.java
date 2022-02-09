4bee0e8286661a8ee2cee44a5b22df389a95c88c
Jain <Nilesh.Jain@sabre.com>
2022-02-10 00:28:08 +0530

    added methods file

---
+++ src/methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 32;
+        System.out.println(addTen(a,b));
+    }
+    public static int addTen(int a,int b){
+        return a+b+10;
+    }
+}
